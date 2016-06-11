71d8ff0a77680ef8c88faf20cb015adb15722f8a
khoilv <laivankhoi318@gmail.com>
2016-06-11 16:53:05 +0700

    Coding Services

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,14 +13,16 @@
         function ($scope, dbService, OBJECT_STORE_NAME_PRODUCT, OBJECT_STORE_NAME_VENDOR, OBJECT_STORE_NAME_TYPE) {
             dbService.initDb();
             $scope.items = null;
+            $scope.latestItems = null;
             
-            // get all products
+            // get and show all products
             dbService.getAll(OBJECT_STORE_NAME_PRODUCT, function (data) {
                 $scope.items = data;
             });
-            
+
+            // get and show the 5 latest products
             dbService.getLatestItems(function (data) {
-                console.log(data);
+                $scope.latestItems = data;
             });
         }
     ]);

992f8a7a830ce4431e52bc8e87e1c3bb54b38ea7
khoilv <laivankhoi318@gmail.com>
2016-06-11 22:51:05 +0700

    Commit code

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -41,19 +41,15 @@
 
             // get and show the 5 latest products
             dbService.getLatestItems(function (items) {
-                var latestItems = [], latestItem = null;
-                items.forEach(function (item) {
-                    latestItem = {
-                        "photo": item.photo,
-                        "name": item.name,
-                        "price": item.price,
-                        "vendor_logo": null,
-                        "vendor_name": null
-                    };
-                    dbService.getVendor(item.vendor_id, latestItem);
-                    latestItems.push(latestItem);
-                });
-                $scope.latestItems = latestItems;
+                if (Array.isArray(items) && items.length > 0) {
+                    var i, length = items.length, item = null;
+                    for (i = 0; i < length; i++) {
+                        item = items[i];
+                        dbService.getVendor(item.vendor_id, item);
+                        items[i] = item;
+                    }
+                    $scope.latestItems = items;
+                }
             });
         }
     ]);

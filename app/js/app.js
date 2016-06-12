84f3cf4990e87c1c3ad4b0c82818675daba41796
khoilv <laivankhoi318@gmail.com>
2016-06-13 00:09:38 +0700

    Commit code

--- app/js/app.js
+++ app/js/app.js
@@ -28,7 +28,7 @@
         function($locationProvider, $routeProvider, $indexedDBProvider) {
             $locationProvider.hashPrefix('!');
             //$locationProvider.html5Mode(false).hashPrefix('!');
-            
+
             $routeProvider
                 .when('/', {
                     templateUrl: 'partials/home/main.html',
@@ -48,33 +48,35 @@
                 .connection('myIndexedDB')
                 .upgradeDatabase(1, function (event, db, tx) {
 
+                    var objStore = null;
+
                     // vendor
                     if (!db.objectStoreNames.contains("vendor")) {
-                        var vendorObjStore = db.createObjectStore('vendor', {keyPath: 'id', autoIncrement: true});
-                        vendorObjStore.createIndex('name_idx', 'name', {unique: true});
-                        vendorObjStore.createIndex('logo_idx', 'logo', {unique: false});
+                        objStore = db.createObjectStore('vendor', {keyPath: 'id', autoIncrement: true});
+                        objStore.createIndex('name_idx', 'name', {unique: true});
+                        objStore.createIndex('logo_idx', 'logo', {unique: false});
                     }
 
                     // type
                     if (!db.objectStoreNames.contains("type")) {
-                        var typeObjStore = db.createObjectStore('type', {keyPath: 'id', autoIncrement: true});
-                        typeObjStore.createIndex('name_idx', 'name', {unique: true});
+                        objStore = db.createObjectStore('type', {keyPath: 'id', autoIncrement: true});
+                        objStore.createIndex('name_idx', 'name', {unique: true});
                     }
 
                     // product
                     if (!db.objectStoreNames.contains("product")) {
-                        var productObjStore = db.createObjectStore('product', {keyPath: 'id', autoIncrement: true});
-                        productObjStore.createIndex('name_idx', 'name', {unique: false});
-                        productObjStore.createIndex('vendor_id_idx', 'vendor_id', {unique: false});
-                        productObjStore.createIndex('type_id_idx', 'type_id', {unique: false});
-                        productObjStore.createIndex('serial_number_idx', 'serial_number', {unique: false});
-                        productObjStore.createIndex('price_idx', 'price', {unique: false});
-                        productObjStore.createIndex('weight_idx', 'weight', {unique: false});
-                        productObjStore.createIndex('color_idx', 'color', {unique: false});
-                        productObjStore.createIndex('release_date_idx', 'release_date', {unique: false});
-                        productObjStore.createIndex('published_idx', 'published', {unique: false});
-                        productObjStore.createIndex('photo_idx', 'photo', {unique: false});
-                        productObjStore.createIndex('created_date_idx', 'created_date', {unique: false});
+                        objStore = db.createObjectStore('product', {keyPath: 'id', autoIncrement: true});
+                        objStore.createIndex('name_idx', 'name', {unique: false});
+                        objStore.createIndex('vendor_id_idx', 'vendor_id', {unique: false});
+                        objStore.createIndex('type_id_idx', 'type_id', {unique: false});
+                        objStore.createIndex('serial_number_idx', 'serial_number', {unique: false});
+                        objStore.createIndex('price_idx', 'price', {unique: false});
+                        objStore.createIndex('weight_idx', 'weight', {unique: false});
+                        objStore.createIndex('color_idx', 'color', {unique: false});
+                        objStore.createIndex('release_date_idx', 'release_date', {unique: false});
+                        objStore.createIndex('published_idx', 'published', {unique: false});
+                        objStore.createIndex('photo_idx', 'photo', {unique: false});
+                        objStore.createIndex('created_date_idx', 'created_date', {unique: false});
                     }
                 });
         }

73910b9b8dc949d9336cb12de796b44723d56d75
khoilv <laivankhoi318@gmail.com>
2016-06-12 12:00:05 +0700

    Coding Inventory Management App with AngularJS + IndexedDB

--- app/js/filters.js
+++ app/js/filters.js
@@ -5,11 +5,45 @@
     var imFilters = angular.module('imFilters', []);
 
     imFilters.filter('weight', function () {
-        return function (w) {
+        return function (w, unit) {
             if (isNaN(w) || w < 0) {
                 return w;
             } else {
-                return w + ' g';
+                if (unit === undefined) unit = 'g';
+                return w + ' ' + unit;
+            }
+        }
+    });
+
+    /**
+     * Build our own custom currency filter supports the ability to choose what side the currency symbol goes on.
+     * For our custom filter, we will allow the user to pass two parameters.
+     * The first will be the symbol or string they want to use to denote the currency,
+     * and second a true or false boolean value that will determine whether the symbol is added before or after the amount.
+     *
+     * Usage: {{ 25 | customCurrency:'$':false }}
+     */
+    imFilters.filter('customCurrency', function() {
+
+        // Create the return function and set the required parameter name to **input**
+        // setup optional parameters for the currency symbol and location (left or right of the amount)
+        return function(input, symbol, place) {
+
+            // Ensure that we are working with a number
+            if(isNaN(input)) {
+                return input;
+            } else {
+
+                // Check if optional parameters are passed, if not, use the defaults
+                var symbol = symbol || '$';
+                var place = place === undefined ? true : place;
+
+                // Perform the operation to set the symbol in the right location
+                if( place === true) {
+                    return symbol + input;
+                } else {
+                    return input + symbol;
+                }
             }
         }
     });

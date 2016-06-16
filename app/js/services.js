d50102930e41ea5ed82bda0caff865ef6f83afab
khoilv <laivankhoi318@gmail.com>
2016-06-16 17:38:33 +0700

    Commit code

--- app/js/services.js
+++ app/js/services.js
@@ -52,12 +52,12 @@
                 
                 // initialize products
                 var products = [
-                    {"name": "Laptop HP Envy 10", "vendor_id": 1, "type_id": 1, "serial_number": "123456", "price": "18", "weight": "200", "color": "Black", "release_date": convertDate('06/10/2016'), "published": "1", "photo": "img/product/p-img1.jpg", "created_date": convertDate('06/10/2016')},
-                    {"name": "Laptop Dell Inspiration 6", "vendor_id": 2, "type_id": 1, "serial_number": "123456", "price": "30", "weight": "200", "color": "White", "release_date": convertDate('06/12/2016'), "published": "1", "photo": "img/product/p-img2.jpg", "created_date": convertDate('06/06/2016')},
-                    {"name": "Laptop HP Envy 11", "vendor_id": 3, "type_id": 2, "serial_number": "123456", "price": "95", "weight": "200", "color": "White", "release_date": convertDate('06/08/2016'), "published": "1", "photo": "img/product/p-img3.jpg", "created_date": convertDate('06/11/2016')},
-                    {"name": "Laptop HP Envy 8", "vendor_id": 4, "type_id": 2, "serial_number": "123456", "price": "52", "weight": "200", "color": "Black", "release_date": convertDate('05/28/2016'), "published": "1", "photo": "img/product/p-img4.jpg", "created_date": convertDate('06/08/2016')},
-                    {"name": "Laptop HP Envy 9", "vendor_id": 5, "type_id": 3, "serial_number": "123456", "price": "18", "weight": "200", "color": "Black", "release_date": convertDate('06/01/2016'), "published": "1", "photo": "img/product/p-img4.jpg", "created_date": convertDate('06/09/2016')},
-                    {"name": "Laptop HP Envy 7", "vendor_id": 1, "type_id": 3, "serial_number": "123456", "price": "27.25", "weight": "200", "color": "Black", "release_date": convertDate('05/30/2016'), "published": "1", "photo": "img/product/p-img4.jpg", "created_date": convertDate('06/07/2016')}
+                    {"name": "Laptop HP Envy 10", "vendor_id": 1, "type_id": 1, "serial_number": "123456", "price": "18", "weight": "200", "color": "Black", "release_date": convertDate('06/10/2016'), "published": true, "photo": "img/product/p-img1.jpg", "created_date": convertDate('06/10/2016')},
+                    {"name": "Laptop Dell Inspiration 6", "vendor_id": 2, "type_id": 1, "serial_number": "123456", "price": "30", "weight": "200", "color": "White", "release_date": convertDate('06/12/2016'), "published": false, "photo": "img/product/p-img2.jpg", "created_date": convertDate('06/06/2016')},
+                    {"name": "Laptop HP Envy 11", "vendor_id": 3, "type_id": 2, "serial_number": "123456", "price": "95", "weight": "200", "color": "White", "release_date": convertDate('06/08/2016'), "published": true, "photo": "img/product/p-img3.jpg", "created_date": convertDate('06/11/2016')},
+                    {"name": "Laptop HP Envy 8", "vendor_id": 4, "type_id": 2, "serial_number": "123456", "price": "52", "weight": "200", "color": "Black", "release_date": convertDate('05/28/2016'), "published": true, "photo": "img/product/p-img4.jpg", "created_date": convertDate('06/08/2016')},
+                    {"name": "Laptop HP Envy 9", "vendor_id": 5, "type_id": 3, "serial_number": "123456", "price": "18", "weight": "200", "color": "Black", "release_date": convertDate('06/01/2016'), "published": true, "photo": "img/product/p-img4.jpg", "created_date": convertDate('06/09/2016')},
+                    {"name": "Laptop HP Envy 7", "vendor_id": 1, "type_id": 3, "serial_number": "123456", "price": "27.25", "weight": "200", "color": "Black", "release_date": convertDate('05/30/2016'), "published": true, "photo": "img/product/p-img4.jpg", "created_date": convertDate('06/07/2016')}
                 ];
                 $indexedDB.openStore(OBJECT_STORE_NAME_PRODUCT, function (store) {
                     store.clear().then(function () { });
@@ -71,7 +71,7 @@
                         var data = [];
                         if (Array.isArray(items) && items.length > 0) {
                             angular.forEach(items, function (item) {
-                               if (item.published == 1) data.push(item);
+                               if (item.published == true) data.push(item);
                             });
                             if (typeof callback === 'function') {
                                 callback(data);
@@ -86,7 +86,7 @@
                     store.getAll().then(function (items) {
                         var data = [], searchKey = null;
                         angular.forEach(items, function (item) {
-                            if (item.published == 1) {
+                            if (item.published == true) {
                                 searchKey = item.name + ' ' + item.color + ' ' + item.price;
                                 if (searchKey.toLowerCase().search(searchValue.toLowerCase()) != -1) {
                                     data.push(item);
@@ -107,7 +107,7 @@
                         var data = [];
                         if (Array.isArray(e) && e.length > 0) {
                             angular.forEach(e, function (item) {
-                               if (item.published == 1) data.push(item);
+                               if (item.published == true) data.push(item);
                             });
                             if (data.length > 5) data = data.slice(0, 5);
                             if (typeof callback === 'function') callback(data);
